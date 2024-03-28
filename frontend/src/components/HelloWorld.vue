55f54da2408310e2cb6715ab91263c8178a7133b
lotfimay <lotfimayouf09@gmail.com>
2024-03-28 16:05:51 +0100

    added code

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1 class="font-bold text-xl text-red-500">Hello From GROUPE-07</h1>
+  <h1>Hello From GROUPE-07</h1>
   <p>Testing the CI/CD</p>
-  <p>New code here</p>
 </template>
+
+<style scoped></style>
