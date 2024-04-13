e517df94dd591a844aabb52e98dc79080ed41f18
lotfimay <lotfimayouf09@gmail.com>
2024-04-13 16:17:35 +0200

    setup shadcn library

--- frontend/src/components/auth/LoginForm.vue
+++ frontend/src/components/auth/LoginForm.vue
@@ -2,48 +2,58 @@
 import CardWrapper from "../ui/CardWrapper.vue";
 import { LoginSchema } from "../../validation/auth";
 import { Form, Field, ErrorMessage } from "vee-validate";
+import { Button } from "@/components/ui/button/index.js";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card/index";
 </script>
 
 <template>
-  <CardWrapper
-    header-title="Welcome Back"
-    back-button-label="Don't you have an account ?"
-    back-button-url="/"
-  >
-    <Form
-      action=""
-      @submit.prevent="submitLogin"
-      :validation-schema="LoginSchema"
-    >
-      <div class="w-full my-8">
-        <label for="email" class="block">Email</label>
-        <Field
-          id="email"
-          name="email"
-          type="email"
-          class="w-full rounded-sm p-2 outline-none focus-visible:border focus-visible:border-[#03C988]"
-        />
-        <ErrorMessage name="email" class="text-red-500" />
-
-        {{}}
-      </div>
-      <div class="w-full my-8">
-        <label for="password" class="block">Password</label>
-        <Field
-          id="password"
-          name="password"
-          type="password"
-          class="w-full rounded-sm p-2 outline-none focus-visible:border focus-visible:border-[#03C988]"
-        />
-        <ErrorMessage name="password" class="text-red-500" />
-      </div>
-      <button
-        class="w-full p-2 text-white font-semibold rounded-sm bg-[#03C988]"
+  <Card class="w-1/3 m-auto">
+    <CardHeader class="text-center">
+      <CardTitle>Welcome Back !</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <Form
+        action=""
+        @submit.prevent="submitLogin"
+        :validation-schema="LoginSchema"
       >
-        Login
-      </button>
-    </Form>
-  </CardWrapper>
+        <div class="w-full my-8">
+          <label for="email" class="block">Email</label>
+          <Field
+            id="email"
+            name="email"
+            type="email"
+            class="w-full rounded-sm p-2 outline-none border focus-visible:border-[#03C988]"
+          />
+          <ErrorMessage name="email" class="text-red-500" />
+
+          {{}}
+        </div>
+        <div class="w-full my-8">
+          <label for="password" class="block">Password</label>
+          <Field
+            id="password"
+            name="password"
+            type="password"
+            class="w-full rounded-sm p-2 outline-none border focus-visible:border-[#03C988]"
+          />
+          <ErrorMessage name="password" class="text-red-500" />
+        </div>
+        <Button class="w-full font-semibold"> Login </Button>
+      </Form>
+    </CardContent>
+    <CardFooter class="w-fit m-auto">
+      <RouterLink class="underline" to="/"
+        >Don't you have an account ?</RouterLink
+      >
+    </CardFooter>
+  </Card>
 </template>
 
 <script>
