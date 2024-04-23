7d96d0c9f656e720f723b2a0cdd14766f94e3f1b
THINOT EDOUARD p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2024-04-23 14:35:08 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,10 +17,8 @@ load_dotenv()                    #load the root file ".env" ATTENTION YOU NEED T
 
 import os 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
