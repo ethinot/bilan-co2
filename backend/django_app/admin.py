a4670a6250cd6299942aef13e8f87566ef832988
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2024-04-24 11:22:29 +0200

    MAJ - rectification de admin.py

--- backend/django_app/admin.py
+++ backend/django_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django_app.models import User_data
+from django_app.models import *
 
 admin.site.register(User_data)
+admin.site.register(Consommation)
