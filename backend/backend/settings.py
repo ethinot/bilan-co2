a884f80f636e3d4cd90184b5341642a163a16659
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2024-04-26 14:32:52 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production! 
-# TODO : Passed to false for the final deploiement!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '*']
 
