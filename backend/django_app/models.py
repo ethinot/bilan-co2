73563db87427d442905168eb0c7786a2bf11db3c
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2024-04-29 15:07:38 +0200

    Delete TestModel class.

--- backend/django_app/models.py
+++ backend/django_app/models.py
@@ -75,10 +75,3 @@ class Consommation(models.Model):
     def __str__(self):
         return str(self.nom_produit)   
     
-class TestModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
