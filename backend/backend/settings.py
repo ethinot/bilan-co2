5abab7804b057979a4b68fe84015a00bc81395fc
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2024-04-29 14:54:49 +0200

    Passe the debug value to true.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production! 
-DEBUG = False   
+DEBUG = True   
 
 ALLOWED_HOSTS = ['localhost', '*']
 
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'emissiontracker',
-#         'USER': os.environ.get('DATABASE_USERNAME'),
-#         'PASSWORD': os.environ.get('DATABASE_USERPASSWORD'),
-#         'HOST': '192.168.75.83',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'emissiontracker',
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_USERPASSWORD'),
+        'HOST': '192.168.75.83',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
