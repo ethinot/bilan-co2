6e2f285e97ec9c5ccf3818f8deb668a9086060b6
lotfimay <lotfimayouf09@gmail.com>
2024-04-30 15:08:24 +0200

    modified settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,16 +93,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "emissiontracker",
-#         "USER": os.environ.get("DATABASE_USERNAME"),
-#         "PASSWORD": os.environ.get("DATABASE_USERPASSWORD"),
-#         "HOST": "127.0.0.1",
-#         "PORT": "",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "emissiontracker",
+        "USER": os.environ.get("DATABASE_USERNAME"),
+        "PASSWORD": os.environ.get("DATABASE_USERPASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "",
+    }
+}
 
 
 # DATABASES = {
@@ -112,12 +112,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 #     }
 # }
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Password validation
