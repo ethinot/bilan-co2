5edbde5473c4f41dce5d5c2d8f97ea6acd3838e8
amineitji <amineitji@gmail.com>
2024-04-30 13:24:45 +0200

    Merge entre django-models-api et gestion du CSV

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
+#DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
--- backend/django_app/tests.py
+++ backend/django_app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User_data, Consommation
+from .models import User_data, Consommation, BD, Alimentation
 from django.contrib.auth.models import User
 from datetime import date
 
@@ -38,5 +38,42 @@ class ConsommationModelTestCase(TestCase):
         self.assertEqual(self.consommation.frequence_utilisation, 'JOURNALIER')
         self.assertEqual(self.consommation.quantite_co2, 10.0)
         self.assertEqual(self.consommation.type_consommation, 'FOSSILE')
+
+class TransportTestCase(TestCase):
+    def setUp(self) -> None:
+        self.test = BD("transport.csv")
+     
+    def test_calcul(self):
+        self.assertEqual(self.test.calcul("TER",100), 100*0.03)
+    def test_calcul_gestion_erreur(self):
+        with self.assertRaises(ValueError):
+            self.test.calcul("fusée",300000)
+
+class EnergieTestCase(TestCase):
+    def setUp(self) -> None:
+        self.test = BD("energie.csv")
+
+    def test_calcul(self):
+        self.assertEqual(self.test.calcul("Électricité(centrale nucléaire)",100),100*0.006)
+    def test_calcul_gestion_erreur(self):
+        with self.assertRaises(ValueError):
+            self.test.calcul("tachyon",1000000)
+
+
+class AlimentationTestCase(TestCase):
+    def setUp(self) -> None:
+        self.test = Alimentation("alimentation.csv")
+    def test_calcul(self):
+        self.assertEqual(self.test.calcul("Agar (algue), cru",100),100*0.0389)
+    def test_calcul_gestion_erreur(self):
+        with self.assertRaises(ValueError):
+            self.test.calcul("Gloubi-boulga",1000000)
+    def test_categorie(self):
+        quantite = 10
+        produit = "Eau minérale Rozana, embouteillée, gazeuse, fortement minéralisée (Beauregard, 63)"
+        calcul_sans_selection =self.test.calcul(produit, quantite)
+        self.test.select_categorie("boissons")
+        self.test.select_sous_categorie("eaux")
+        self.assertEqual(calcul_sans_selection,self.test.calcul(produit,quantite))
 
  
